2b91c69b3c200a9bd48caa0e55bf9e76de5d9192
Chris Papastefanou <papastefanou@gmail.com>
2019-10-13 22:29:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <html>
+    <title>Python Lab</title>
 <body>
 
 <h1>python-lab/index.php</h1>    
 
+PHP says:<br>
 <?php
 // PHP code goes here
-print "Yrb";
+print " Yrb";
 ?>
+<br>
+<br>
+Python says:<br>
 
 
+<?php
+$output = shell_exec("python Erb.py");
+var_dump($output);
+?>
 </body>
 
 
